6b78bbe6793dca9fd4525e3810211796e3ef479a
Andres Picazo Cuesta <apicazo@aubay.es>
2013-04-09 11:38:17 +0200

    Version inicial del componente (debug)

--- components/map.js
+++ components/map.js
@@ -3,24 +3,83 @@ define(
     'components/component_manager'
   ],
 
-  function(ComponentManager) {
+function(ComponentManager) {
 
     return ComponentManager.create('map', Map);
 
     function Map() {
+
+	    var mapC = null;
+
+        this.defaultAttrs({
+		    mapId: 'keithtid.map-w594ylml',
+            center: { lat: 40.414431, lon: -3.703696 },
+			zoomInitial: 15,
+            zoomMin: 15,
+            zoomMax: 20,
+			hostDiv: 'dashmap',
+			markers: []
+        });
       
-      this.defaultAttrs({
-        center: { lat: 0, lon: 0},
-        zoomMin: 1,
-        zoomMax: 14
-      });
-      
-      this.after('initialize', function() {
-        
-        this.$node.addClass('map');
-        this.$node.html(this.attr.contenido);
-        
-      });
-    }
-  }
-);
+        this.after('initialize', function() {
+            // Creating map	
+			this.mapC = mapbox.map(this.attr.hostDiv);
+			this.mapC.addLayer(mapbox.layer().id(this.attr.mapId));
+			this.mapC.setZoomRange(this.attr.zoomMin, this.attr.zoomMax);
+			this.centerZoom(this.attr.center, this.attr.zoomInitial);
+			console.log("Created map: " + this.attr.mapId);
+
+			// marker layer
+			this.markerLayer = mapbox.markers.layer();
+			mapbox.markers.interaction(this.markerLayer);
+			this.mapC.addLayer(this.markerLayer);	
+			console.log('Created marker layer');				
+
+				
+			var len = this.attr.markers.length;
+			for (var i = 0; i < len; i++) {
+				if (i in this.attr.markers) {
+					this.addMarkerToMap(this.attr.markers[i]);
+				}
+			}
+			console.log('Created initial markers');			
+        });
+
+		this.centerZoom = function(position,zoom) {
+			this.mapC.centerzoom(position,zoom);
+		};
+
+		this.addMarkerToMap = function(data) {
+            // Just in case some fields are not there
+			data.color = data.color == null ? '#000' : data.color;
+            data.title = data.title == null ? '' : data.title;
+            data.description = data.description == null ? '' : data.description;
+
+			console.log(
+                'Event: lat:' + data.lat + 
+                ' lon:' + data.lon + 
+                ' color: ' + data.color +
+                ' title: ' + data.title +
+                ' description: ' + data.description);
+
+			this.markerLayer.add_feature(
+				{
+				geometry: { coordinates: [data.lon, data.lat] },
+				properties: {
+					'marker-color': data.color,
+					'marker-symbol': 'star-stroked',
+					title: 'Example marker',
+					description: 'Example description'
+					}
+				}
+			);
+		};
+        	
+        this.on(
+            'addMarker', 
+            function () {
+                console.log('===');    
+            }
+        );
+    } 
+});
--- examples/mapdashboard/main.js
+++ examples/mapdashboard/main.js
@@ -1,29 +1,61 @@
+
 requirejs.config({
-  baseUrl: '/m2m-nitro-components'
+    baseUrl: '/m2m-nitro-components'
 });
 
 define(
-  [
+    [
     'components/dashboard/dashboard',
     'components/map'
-  ],
+    ],
   
-  function() {
-  
-    requirejs(['components/jquery_plugins'], function() {
+    
+    function() {
 
-      $('.dashboard').m2mdashboard({
-        mainContent: [{
-          component: 'map',
-          contenido: 'sdlfjsldjkflsdjfsdf'
-        }],
-        overviewPanel: {},
-        data: function() {
+        requirejs(['components/jquery_plugins'], function() {
+            
+        $('.dashboard').m2mdashboard({
+            mainContent: [{
+                    component: 'map',
+                    contenido: 'Contenido de ejemplo',
+				    center: { lat: 40.515512, lon: -3.65 }, 
+	  			    zoomInitial: 14,
+				    zoomMin: 1,
+				    zoomMax: 22,
+                    hostDiv: 'dashmap',
+				    markers: [
+				        { lat:40.5155,lon:-3.6650 }, 
+					    { lat:40.5125, lon:-3.6660, color:'#DC1010' },
+					    { lat:40.51, lon:-3.665, color:'#0040FF' }
+				    ]
+                }],
+                overviewPanel: {},
+                data: function() {}
+            });
+        });
 
-        }
-      });
+        $('#testButton').on(
+            'click', 
+            function () {
+                console.log("Test button clicked");
+                $(this).trigger('triggerTest');
+                //$('#-zoom-14').trigger('triggerTest');
+                //console.log($('#-zoom-14').html());
+            }
+        );
 
-    });
-  }
-
+        $('#testButton').on(
+            'addMarker', 
+            function () {
+                console.log('this works');
+            }
+        );
+       
+        $('#testButton').on(
+            'triggerTest', 
+            function () {
+                console.log('Main.js received event :"triggerTest" at ' + Date());
+            }
+        );
+    }
 );

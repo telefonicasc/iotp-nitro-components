151db53331b661cafad038afd1bbac840eef6943
Andres Picazo Cuesta <apicazo@aubay.es>
2013-04-12 11:46:03 +0200

    Initial working version (off-sight markers and dynamic marker model update remaining)

--- components/map.js
+++ components/map.js
@@ -1,9 +1,75 @@
 /* 
- * Mapbox generic map component
+ * Mapbox generic map component for M2M-Nitro
+ *
+ * Andres Picazo Cuesta 
+ * Telefonica I+D
  *
  * Marker symbols: http://mapbox.com/maki/
  * Mapbox: www.mapbox.com
- * 
+ *
+ * ==> Configuration:
+ * - mapId: mapbox map identifier  
+ * - center [default:{ lat: 40.414431, lon: -3.703696 }] => Starting center point, with format {lat: <value>, lon: <value>} 
+ * - zoomInitial [default:15] => The initial zoom value (recomended 14)
+ * - zoomMin [default:15 ]=> Minimum zoom value
+ * - zoomMax [default:20] => Maximum zoom value
+ * - showZoomButtons [default:false] => If true, show the buttons to zoomin / zoomout
+ * - hoveringTooltip [default: true] => Show tooltip when clicked (false), or hovered (true)  
+ * - centerOnClick [default: false] => Center map on marker when clicked
+ * - markers [default:[]] => Array of markers (see below for description)
+ * - marker_click_event [default:'marker-clicked'] => flight event to trigger when a marker is clicked
+ * - unSelectedSymbol [default: 'circle-stroked']: symbol to assign marker when not selected
+ * - selectedSymbol[default:'circle']: symbol to assign marker when selected
+ * - unselectedColor[default:#0000FF]: color to assign marker when not selected
+ * - selectedColor[default:#00FF00]: color to assign marker when selected
+ * - debug [default:false] => enables console log output (default: false)
+ *
+ * ==> Marker description (example):
+ * {   
+ *  geometry: { 
+ *      coordinates: [ -3.665, 40.515] },
+ *      properties: {
+ *          'id': 1,
+ *          'marker-color':'#000',
+ *          'marker-symbol':'police',
+ *          'marker-size': small,
+ *          'title': 'Marker 1',
+ *          'description': 'Marker 1 description',
+ *          'status': 'ok'
+ *      }                        
+ * }
+ *
+ * For available symbol descriptions, see: http://mapbox.com/maki
+ * (Examples are: 'star', 'star-stroked', 'circle', 'circle-stroked'...)
+ *
+ * ==> Flight events:
+ * [SENT] 
+ * config.marker_click_event :: Sent when a marker is clicked
+ *
+ * [RECEIVED]
+ * 'update-marker': Updates marker view
+ *
+ *
+ * Usage notes:
+ *
+ * Zoomer buttons require to have an associated css to be seen, bare minimum would be:
+ *
+ *      .zoomer {
+ *          position:absolute;
+ *      }
+ *
+ *      .zoomer.zoomin {
+ *          left:10px;
+ *      }
+ *
+ * or thought jquery:
+ *
+ *      $('.zoomer').css('position','absolute');
+ *      $('.zoomer.zoomin').css('left','10px');
+ *
+ * ==> To sent an event (example):
+ *
+ * $('.mapbox').trigger('update-marker',{title: 'Marker 1'});
  */
 
 define(
@@ -17,24 +83,37 @@ function(ComponentManager) {
 
     function Map() {
 
+        /* === Mapbox map component instance === */
 	    var mapC = null;
+        /* === Self pointer to call from functions === */
         var self = null;
 
+        /* === Component default attributes === */
         this.defaultAttrs({
 		    mapId: 'keithtid.map-w594ylml',
             center: { lat: 40.414431, lon: -3.703696 },
 			zoomInitial: 15,
             zoomMin: 15,
             zoomMax: 20,
+            showZoomButtons: false,
+            features: [],
 			markers: [],
             marker_click_event: 'marker-clicked',
+            hoveringTooltip: true,
+            centerOnClick: false,
+            unselectedSymbol: 'circle-stroked',
+            unselectedColor: '#0000FF',
+            selectedSymbol: 'circle',
+            selectedColor: '#00FF00',
+            selected: 0,
             debug: false
         });
       
-        // ==================================================================
+        /* === Component intializer (after) === */
         this.after('initialize', function() {
+            
             self = this;
-            // Creating map 	
+            // ==> Create map 	
             this.$node.addClass('fit');
             this.$nodeMap = $('<div>').addClass('mapbox').appendTo(this.$node);
 			this.mapC = mapbox.map(this.$nodeMap[0]);
@@ -42,80 +121,121 @@ function(ComponentManager) {
 			this.mapC.setZoomRange(this.attr.zoomMin, this.attr.zoomMax);
 			this.mapC.centerzoom(this.attr.center, this.attr.zoomInitial);
 
-			// marker layer
+            // ==> Add zoom buttons
+            if (this.attr.showZoomButtons) {
+                this.mapC.ui.zoomer.add();
+            }            
+
+			// ==> Create marker layer
 			this.markerLayer = mapbox.markers.layer();
+                //.features(this.attr.features)
+                //.key(function (f) { return f.properties.title });
 			var interaction = mapbox.markers.interaction(this.markerLayer);
 			this.mapC.addLayer(this.markerLayer);
+            // ==> Remove hovering tooltips?
+            if (this.attr.hoveringTooltip == false) {
+                interaction.showOnHover(false);
+            } 
+            // ==> Create markers
+            // All markers unselected
+            for (var i = 0; i < self.attr.features.length; i++) {
+                self.attr.features[i].properties['marker-color'] = self.attr.unselectedColor;
+                self.attr.features[i].properties['marker-symbol'] = self.attr.unselectedSymbol;
+            }
 
-            // Put markers	
-			var len = this.attr.markers.length;
-			for (var i = 0; i < len; i++) {
-				if (i in this.attr.markers) {
-					var feature = this.addMarkerToMap(this.attr.markers[i]);
-				}
-			}
-           
-            // Add trigger for the markers
+            // Check this: http://mapbox.com/mapbox.js/example/marker-movement/
+            this.markerLayer.features(this.attr.features);
+            //    .key(function (f) {return f.properties.id} );
+            // ==> On click event trigger for the markers 
             this.markerLayer.factory(
                 function(model) {
                     var elem = mapbox.markers.simplestyle_factory(model);
-                    MM.addEvent(
-                        elem,
-                        'click',
+                    MM.addEvent(elem, 'click',
                         function () {
-                            // Only for this
-                            console.log("layer click: " + elem.alt);
-                            self.trigger('echo');
-                            self.trigger(document, 'triggerTest');
-                            self.trigger(document, 'echo-test');
+                            // Only for test
+                            if (self.attr.debug) {
+                                console.log("layer click: " + elem.alt);
+                                self.trigger('echo');
+                                self.trigger(document, 'echo-test');
+                            }
+                            // Update markers
+                            self.trigger('update-marker-views', {title:elem.alt});
                             // Trigger the event specified
-                            self.trigger(self.attr.marker_click_event);
+                            self.trigger(self.attr.marker_click_event, {text: elem.alt});
                         } 
                     );
+                    // Elem se ha actualizado, por lo que este nodo ya no existe!!
                     return elem;
                 }
             );
+ 
+            if (self.attr.debug) {
+                // ==> Event suscriptions and handlers
 
-            // ---------------------
-            // Trigger subscriptions
-            // ---------------------
-            this.on(
-                'echo',
-                function () {
-                    console.log('Map.js  :: trigger["echo"]');
+                // TEST
+                this.on(this.attr.marker_click_event, function (event,payload) {
+                    console.log('Map.js  :: trigger["' + 
+                        this.attr.marker_click_event + 
+                        '"] Source: ' +
+                        payload.text);
+                });
+            }
+
+            this.on('update-marker-model', function (event, payload) {
+                var index = self.findMarker(payload.title);
+                console.log('index: ' + index);
+            });
+
+            // => Marker update on click
+            this.on('update-marker-views', function (event, payload) {
+                if (self.attr.debug) console.log('Map.js [trigger] updating markers');
+                var sel = self.attr.selected;
+                i = self.findMarker(payload.title);
+                if (i < 0) {
+                    if (self.attr.debug) console.log('marker not found!!');
+                    return null;
                 }
-            ); 
+                // => Update unselected marker
+                self.attr.features[sel].properties['marker-symbol'] = self.attr.unselectedSymbol;
+                self.attr.features[sel].properties['marker-color'] = self.attr.unselectedColor;
+                self.attr.features[sel].properties['marker-size'] = 'medium';
+                
+                // => Update selected marker
+                self.attr.features[i].properties['marker-symbol'] = self.attr.selectedSymbol;
+                self.attr.features[i].properties['marker-color'] = self.attr.selectedColor;
+                self.attr.features[i].properties['marker-size'] = 'large';
 
-            this.on(
-                this.attr.marker_click_event,
-                function () {
-                    console.log('Map.js  :: trigger["' + this.attr.marker_click_event + '"]');
+                // => Reload features
+                self.markerLayer.features(self.attr.features);
+                // => Center map on selected
+                if (self.attr.centerOnClick) {
+                    // lat/lon come reversed from geometry
+                    var feature_lat = self.attr.features[i].geometry.coordinates[1];
+                    var feature_lon = self.attr.features[i].geometry.coordinates[0];
+                    self.mapC.center({lat: feature_lat, lon: feature_lon});
                 }
-            );
+                // => Update attribute selected
+                self.attr.selected = i;
+            });
 
+            this.findMarker = function (markerid) {
+                var i = 0;
+                var found = false;
+                // Find marker to update
+                while (i < self.attr.features.length && !found) {
+                    var current = self.attr.features[i];
+                    if (self.attr.features[i].properties.title == markerid) {
+                        found = true;
+                    }
+                    else i++;
+                }
+                if (!found) return -1;
+                else return i;
+            }
+            
         }); // </this.after(...,function)>
 
-        // ================================================================== 
-		this.addMarkerToMap = function(data) {
-            // Just in case some fields are not there
-			data.color = data.color == null ? '#000' : data.color;
-            data.title = data.title == null ? '' : data.title;
-            data.description = data.description == null ? '' : data.description;
-            data.symbol = data.symbol == null ? 'star' : data.symbol;
-            // Es realmente una feature, ¿o el layer entero?
-			var feature = this.markerLayer.add_feature(
-				{
-				geometry: { coordinates: [data.lon, data.lat] },
-				properties: {
-					'marker-color': data.color,
-					'marker-symbol': data.symbol,
-					title: data.title,
-					description: data.description }
-				}
-			);
-            return feature;
-		}
-        // ================================================================== 
     } // </function Map()>
+
 } // </function(ComponentManager)>
 );
--- examples/mapdashboard/main.js
+++ examples/mapdashboard/main.js
@@ -8,7 +8,6 @@ define(
     'components/dashboard/dashboard',
     'components/map'
     ],
-  
     
     function() {
 
@@ -17,34 +16,51 @@ define(
         $('.dashboard').m2mdashboard({
             mainContent: [{
                 component: 'map',
-                useToolTipFormatter: true,
                 showZoomButtons: true,
+                hoveringTooltip: true,
+                debug: true,
 				center: { lat: 40.515, lon: -3.667 },
-	  			zoomInitial: 14,
-				zoomMin: 1,
-				zoomMax: 22,
-				markers: [
-				    { 
-                        lat:40.515,
-                        lon:-3.665,
-                        symbol: 'police',
-                        title: 'Marker 1'
-                    }, 
-					{ 
-                        lat:40.515, 
-                        lon:-3.670, 
-                        color:'#DC1010',
-                        title: 'Marker 2'
+	  			zoomInitial: 15,
+				zoomMin: 10,
+				zoomMax: 20,
+                unselectedSymbol: 'circle',
+                selectedSymbol: 'star-stroked',
+                unselectedColor: '#0040FF',
+                selectedColor: '#DC1010',
+                centerOnClick: true,
+                features: [
+                    {   
+                        geometry: { coordinates: [ -3.665, 40.515] },
+                        properties: {
+                            'id': 1,
+                            'marker-color':'#000',
+                            'marker-symbol':'police',
+                            'title': 'Marker 1',
+                            'status': 'ok'
+
+                        }                        
                     },
-					{ 
-                        lat:40.515, 
-                        lon:-3.675, 
-                        color:'#0040FF', 
-                        symbol: 'triangle',
-                        title: 'Marker 3',
-                        description: 'This is marker 3'
+                    {
+                        geometry: { coordinates: [ -3.670, 40.515] },
+                        properties: {
+                            'id':2,
+                            'marker-color':'#DC1010',
+                            'marker-symbol':'circle',
+                            'title': 'Marker 2',
+                            'status': 'ok'
+                        }
+                    },
+                    {
+                        geometry: { coordinates: [ -3.675, 40.515] },
+                        properties: {
+                            'id':3,
+                            'marker-color':'#0040FF',
+                            'marker-symbol':'triangle',
+                            'title':'Marker 3',
+                            'status': 'error'
+                        }
                     }
-				]
+                ],
             }],
                 overviewPanel: {},
                 data: function() {}
@@ -55,8 +71,9 @@ define(
             'click', 
             function () {
                 console.log("Test button clicked");
-                $(this).trigger('triggerTest',{text:'token'});
-                $(this).trigger('echo');
+                $(this).trigger('echo-test');
+                $('.mapbox').trigger('update-marker-views',{title: 'Marker 1'});
+                $('.mapbox').trigger('update-marker-model',{title: 'Marker 1'});
             }
         );
                

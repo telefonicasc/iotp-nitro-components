ee9fd0fa857949f4309b4c27ca851d71b291f074
Manuel J. Romero <mjromper@gmail.com>
2013-04-17 17:43:33 +0100

    Better data binding

--- components/chart/group_bar_chart.js
+++ components/chart/group_bar_chart.js
@@ -11,65 +11,62 @@ define(
         function GroupBarChart(){
 
         	this.defaultAttrs({
-        		data: [
-        			{group:'CA' ,values: [2704659,4499890,2159981,3853788,10604510,8819342,4114496]},
-        			{group:'TX' ,values: [2027307,3277946,1420518,2454721,7017731,5656528,2472223]},
-        			{group:'NY' ,values: [1208495,2141490,1058031,1999120,5355235,5120254,2607672]},
-        			{group:'FL' ,values: [1140516,1938695,925060,1607297,4782119,4746856,3187797]},
-        			{group:'IL' ,values: [894368,1558919,725973,1311479,3596343,3239173,1575308]},
-        			{group:'PA' ,values: [737462,1345341,679201,1203944,3157759,3414001,1910571]}
-
-        		],
-        		names: ['Under 5 Years','5 to 13 Years','14 to 17 Years','18 to 24 Years','25 to 44 Years','45 to 64 Years','65 Years and Over'],
-        		colors: ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]
+        		data: {
+	        		values: [
+	        			{ '1 hour': [2704659,4499890,2159981,3853788,10604510,8819342,4114496] },
+	        			{ '4 hours': [2027307,3277946,1420518,2454721,7017731,5656528,2472223] },
+	        			{ '8 hours': [1208495,2141490,1058031,1999120,5355235,5120254,2607672] },
+	        			{ '1 day': [1140516,1938695,925060,1607297,4782119,4746856,3187797] },
+	        			{ '1 month': [894368,1558919,725973,1311479,3596343,3239173,1575308] },
+	        			{ '50 megs': [737462,1345341,679201,1203944,3157759,3414001,1910571] },
+	        			{ '250 megs': [837462,2345341,779201,3203944,4157759,4414001,2910571] }],
+	        		names: ['Under 5 Years','5 to 13 Years','14 to 17 Years','18 to 24 Years','25 to 44 Years','45 to 64 Years','65 Years and Over'],	
+        			colors: ["#d3d2bc", "#dfcab5", "#c5cfc5", "#dfc4b8", "#bfcac9", "#dedcb3", "#cdbcbe"]
+        		}  		
             });
 
             this.after('initialize', function() {
 				
-				var margin = {top: 0, right: 20, bottom: 20, left: 0};
 				var x0 = d3.scale.ordinal().rangeRoundBands([0, this.width], .1),
 					x1 = d3.scale.ordinal(),
 					y = d3.scale.linear().range([this.height, 0]);
 
-				var xAxis = d3.svg.axis().scale(x0).orient("bottom");
-				var yAxis = d3.svg.axis()
-				.scale(y)
-				.orient("left")
-				.tickFormat(d3.format("0.01s"));		
+				var xAxis = d3.svg.axis().scale(x0).orient("bottom"),
+					yAxis = d3.svg.axis()
+					.scale(y)
+					.orient("left")
+					.tickFormat(d3.format("0.01s"));		
 
 				var svg = d3.select(this.node).append("svg")
 				.append("g")
-				.attr("transform", "translate(" + 0 + "," + margin.top + ")");
+				.attr("transform", "translate(" + 0 + "," + 0 + ")");
 
-				var axisX = svg.append("g")
-				.attr("class", "x axis");
+				var axisX = svg.append("g").attr("class", "x axis"),
+					axisY = svg.append("g").attr("class", "y axis");
 
-				var axisY = svg.append("g")
-				.attr("class", "y axis");
-
-				var barGroups = svg.selectAll(".barGroup").data(this.attr.data);
-				
-
+				var barGroups = svg.selectAll(".barGroup").data(this.attr.data.values);
+			
 				this.updateChart = function() {
 					
 					barGroups.remove();
 
-					svg.attr("width", this.width + margin.left + margin.right)
-					.attr("height", this.height + margin.top + margin.bottom)
+					svg.attr("width", this.width).attr("height", this.height);
 					
-					var _data= this.attr.data;
-					var _names= this.attr.names;
-					var _colors= this.attr.colors;
+					var _data = this.attr.data.values;
+					var _names = this.attr.data.names;
+					var _colors = this.attr.data.colors;
 					    
-					_data.forEach(function(d) {
-						d.ages = [];
-						d.values.forEach(function(val, i){
-							d.ages.push({name: _names[i], value: val});
+					_data.forEach(function(obj) {
+						obj.ages = [];
+						var values = obj[Object.keys(obj)[0]];
+						values.forEach(function(val, i){
+							obj.ages.push({name: _names[i], value: val});
 						});
 					});
 
-					x0.domain(_data.map(function(d) { 
-						return d.group; 
+					x0.domain(_data.map(function(d) {
+					    var key = Object.keys(d)[0]; 
+						return key; 
 					}));
 					x1.domain(_names).rangeRoundBands([0, x0.rangeBand()]);
 					y.domain([0, d3.max(_data, function(d) { 
@@ -84,21 +81,23 @@ define(
 					axisY.attr("transform", "translate("+(this.width+20)+"," + 0 + ")")
 					.call(yAxis);
 
-					barGroups = svg.selectAll(".state").data(this.attr.data);
+					barGroups = svg.selectAll(".state").data(_data);
 					barGroups.enter().append("g");
 					
 					barGroups.attr("class", "g")
 					.attr("transform", function(d) { 
-						return "translate(" + x0(d.group) + ",0)"; 
+						var key = Object.keys(d)[0]; 
+						return "translate(" + x0(key) + ",0)"; 
 					});
-					
-					var _height = this.height;
 
+					barGroups.style("fill", function(d,i) { return _colors[i] });
+					
 					var bars = barGroups.selectAll(".bar2")
 					.data(function(d) { 
 						return d.ages; 
 					});
 
+					var _height = this.height;
 					bars.enter().append("rect")
 					.attr('class', 'bar2')
 					.attr("width", x1.rangeBand()-2)
@@ -110,9 +109,6 @@ define(
 					})
 					.attr("height", function(d) { 
 						return _height - y(d.value); 
-					})
-					.style("fill", function(d,i) { 
-						return _colors[i] 
 					});
 					
             	}	
@@ -120,10 +116,8 @@ define(
             	this.on('resize', function(e, chartSize) {
                    this.width = chartSize.width;
                    this.height = chartSize.height;
-                 
                    x0.rangeRoundBands([0, this.width], .1);
-				   y.range([this.height, 0]);
-				   
+				   y.range([this.height, 0]);				   
                    this.updateChart();	
                    e.stopPropagation();
                 });

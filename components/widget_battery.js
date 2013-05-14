fcb778d5a6ee2154c76f47ec8802a99f2152580c
Andres Picazo Cuesta <apicazo@aubay.es>
2013-05-14 14:28:54 +0200

    Added components (to be completed) for battery and pitch. Updated dashboard with them

---
+++ components/widget_battery.js
@@ -0,0 +1,125 @@
+define (
+[
+    'components/component_manager',
+    'libs/raphael/raphael'
+],
+                                
+function (ComponentManager) {
+    
+    return ComponentManager.create('batteryWidget', BatteryWidget);
+                                                        
+    function BatteryWidget () {
+        
+        this.defaultAttrs({
+            detailsTitle: '.details-title',
+            temperatureChart: '.temperature-chart',
+            temperatureLabel: '.temperature-label',
+            pitchChart: '.pitch-chart',
+            pitchLabel: '.pitch-label',
+            fillColor: '#6F8388',
+            borderColor: '#6F8388',
+            baseColor: '#E9EFF0',
+            id: 'battery-widget',
+            tmin: 0,
+            tmax: 20,
+            temp: 15.6
+        });
+                                                                                        
+        this.after('initialize', function () {
+            this.$node.attr('id', this.attr.id);
+            
+            this.$nodeMap = $('<div>').addClass('temperature-label').appendTo(this.$node);
+
+            //var obj = this.createTemperatureChart();
+            this.on('drawTemperature', function () {
+                console.log('Drawing battery widget');
+                this.attr.widget = this.createTemperatureChart(); 
+                this.drawTemperature(this.attr.widget, this.attr.temp);
+                $(this.attr.temperatureLabel).html(this.attr.temp + 'ºC');
+            });
+        });
+
+        // ==========================
+        // [UPDATE] Temperature Chart
+        // ==========================
+        this.drawTemperature = function(temperatureChart,temperature) {
+            var temp;
+            //var tmin = 0;
+            //var tmax = 20;
+            var tmin = this.attr.tmin;
+            var tmax = this.attr.tmax;
+            var tempCelsius = parseFloat(temperature);
+            if(tempCelsius < tmin){
+              temp = tmin;
+            }
+            else if (tempCelsius > tmax){
+              temp = tmax;
+            }
+            else{
+              temp = tempCelsius;
+            }
+            tamano = temp*(tmax-1)/(tmax-tmin);
+
+            temperatureChart.thermoBar.animate({
+              height: tamano,
+              y: 42 - tamano
+            }, 300);
+
+            temperatureChart.thermoBar.show();
+            temperatureChart.thermoBorder.show();
+            temperatureChart.thermoCircle.show();
+        }
+  
+        // ==========================
+        // [CREATE] Temperature Chart 
+        // ==========================    
+        this.createTemperatureChart = function(options) {
+            //var paper = Raphael(this.attr.temperatureChart.get(0),100,60)
+            var paper = Raphael(this.attr.id,100,60)
+              , thermoBar; 
+
+            options = $.extend({
+              thermoColor: this.attr.fillColor, 
+              thermoLines: this.attr.borderColor
+            }, options);
+            
+            var thermoBorder = paper.set();
+            thermoBorder.push(
+              thermoBarBorder = paper.rect(20,21,8,22.5,4),
+              thermoCircleBorder = paper.circle(24,45,6,13), 
+              cover = paper.rect(20.2,40.8,7.5,8)
+            );
+            thermoBorder.attr({
+              stroke: this.attr.borderColor,
+              fill: this.attr.baseColor
+            });
+            cover.attr({
+              'stroke-width':0
+            })
+
+            var mercury = paper.set();
+            mercury.push(
+              thermoBar = paper.rect(22,55, 4, 0),
+              thermoCircle = paper.circle(24,45,4)
+            );
+
+            mercury.attr({
+              fill: options.thermoColor,
+              stroke: options.thermoBar
+            });
+         
+            thermoCircleBorder.toBack();
+            thermoBar.toFront();
+
+            mercury.hide();
+            thermoBorder.hide();
+            return {
+              paper: paper,
+              thermoBar: thermoBar,
+              thermoCircle: thermoCircle,
+              thermoBorder: thermoBorder
+            };
+        }
+    }
+});
+
---
+++ components/widget_pitch.js
@@ -0,0 +1,163 @@
+define (
+[
+    'components/component_manager',
+    'libs/raphael/raphael'
+],
+                                
+function (ComponentManager) {
+    
+    return ComponentManager.create('pitchWidget', PitchWidget);
+                                                        
+    function PitchWidget () {
+        
+        this.defaultAttrs({
+            pitchChart: '.pitch-chart',
+            pitchLabel: '.pitch-label',
+            fillColor: '#6F8388',
+            borderColor: '#6F8388',
+            baseColor: '#E9EFF0',
+            id: 'pitch-widget',
+            angle: 75
+        });
+                                                                                        
+        this.after('initialize', function () {
+            this.$node.attr('id', this.attr.id);
+            
+            this.$nodeMap = $('<div>').addClass('pitch-label').appendTo(this.$node);
+
+            //var obj = this.createTemperatureChart();
+            this.on('drawPitch', function () {
+                console.log('Drawing pitch widget');
+                this.attr.widget = this.createPitchChart(); 
+                this.drawPitch(this.attr.widget, this.attr.angle);
+                $(this.attr.pitchLabel).html(this.attr.angle + 'º');
+            });
+        });
+
+
+        this.createPitchChart = function() {
+            var paper = Raphael(this.attr.id, 100, 60)
+                        , arm; 
+
+            arm = paper.rect(11.5,37, 4, 15.5);
+            smph = paper.rect(8,10,11,27,3);
+            redLight = paper.circle(13.6,16, 2.25);
+            yellowLight =paper.circle(13.6,23.5, 2.25);
+            greenLight = paper.circle(13.6,31, 2.25);
+            base = paper.rect(18,52,21.5,1);
+            line = this.obtainLine(16, 52, 29, 0 , 90, paper);
+            fill = paper.rect(12.5,35,2, 3);
+            coverLine = paper.rect(16,23.5,5,4);
+
+            base.attr({stroke: this.attr.borderColor, "stroke-width":0,'fill-opacity':0});
+            arm.attr({stroke: this.attr.borderColor,'fill-opacity':0});
+            smph.attr({stroke: this.attr.borderColor,'fill-opacity':0}); 
+            redLight.attr({fill: this.attr.fillColor,stroke: this.attr.fillColor});
+            yellowLight.attr({fill: this.attr.fillColor,stroke: this.attr.fillColor});
+            greenLight.attr({fill: this.attr.fillColor,stroke: this.attr.fillColor});
+            fill.attr({fill: this.attr.baseColor, stroke: this.attr.baseColor});
+            coverLine.attr({fill: this.attr.baseColor, stroke: this.attr.baseColor});
+
+            coverLine.toBack();
+
+            redLight.toFront();
+            yellowLight.toFront();
+            greenLight.toFront();
+            line.toBack();
+            base.toBack();
+             arm.toBack();
+
+            greenLight.hide();
+            arm.hide();
+            yellowLight.hide();
+            redLight.hide();
+            smph.hide();
+            base.hide();
+            line.hide();
+
+            return {
+                base: base,
+                redLight: redLight,
+                yellowLight: yellowLight,
+                greenLight: greenLight,
+                smph: smph,
+                paper: paper,
+                arm: arm,
+                line:line,
+                fill: fill,
+                coverLine: coverLine
+            };
+        }
+
+        this.drawPitch = function(pitchChart, angle) {
+            xmin = 15.5;
+            ymax = 52.5;
+            r = 27; 
+
+            if (angle > 90){
+                angle = abs(angleSmph-180);
+            }
+            if (angle < 10) {
+                angle = 10;
+            }
+            
+            angleSmph = -angle + 90;
+
+            x1 = xmin + r * Math.cos(-0 * Math.PI / 180),
+            x2 = xmin + r * Math.cos(-angle * Math.PI / 180),
+            y1 = ymax + r * Math.sin(-0 * Math.PI / 180),
+            y2 = ymax + r * Math.sin(-angle * Math.PI / 180);
+
+            pitchChart.greenLight.animate({
+              transform: "r" + angleSmph +","+xmin + ","+ ymax
+            },500);
+
+            pitchChart.yellowLight.animateWith(pitchChart.smph,null,{
+              transform: "r" + angleSmph +","+xmin + ","+ ymax
+
+            },500);
+            pitchChart.redLight.animateWith(pitchChart.smph,null,{
+              transform: "r" + angleSmph +","+xmin + ","+ ymax
+            },500);
+            pitchChart.arm.animateWith(pitchChart.smph,null,{
+              transform: "r"+ angleSmph +","+xmin + ","+ ymax
+            },500);
+            pitchChart.smph.animateWith(pitchChart.smph,null,{
+              transform: "r"+angleSmph+","+xmin + ","+ ymax
+            },500);
+            pitchChart.fill.animateWith(pitchChart.smph,null,{
+              transform: "r"+angleSmph+","+xmin + ","+ ymax
+            },500);
+            pitchChart.coverLine.animateWith(pitchChart.smph,null,{
+              transform: "r"+angleSmph+","+xmin + ","+ ymax
+            },500);
+            pitchChart.line.animateWith(pitchChart.smph,null,{
+              path: ["M", x1, y1, "A", r, r, 0, 0, 0, x2, y2],
+              stroke: this.attr.borderColor
+            },500);
+            
+            //Show elements
+            pitchChart.smph.show();
+            pitchChart.base.show();
+            pitchChart.arm.show();
+            pitchChart.redLight.show();
+            pitchChart.yellowLight.show();
+            pitchChart.greenLight.show();
+            //this.pitchChart.line.show();
+            this.attr.widget.line.show();
+          }
+
+        this.obtainLine = function(cx, cy, r, startAngle, endAngle, paper) {
+            x1 = cx + r * Math.cos(-startAngle * Math.PI / 180),
+            x2 = cx + r * Math.cos(-endAngle * Math.PI / 180),
+            y1 = cy + r * Math.sin(-startAngle * Math.PI / 180),
+            y2 = cy + r * Math.sin(-endAngle * Math.PI / 180);
+            x =paper.path(["M", x1, y1, "A", r, r, 0, 0, 0, x2, y2]).attr({
+                stroke: this.attr.borderColor
+            });//, 'stroke-miterlimit': "decimal"});
+            return x;
+        }
+
+    } // </PitchWidget>
+});
+

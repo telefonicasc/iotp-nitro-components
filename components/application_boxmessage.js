6758f7462b5460c99ed5b1d2d1de5ffc2f2c3b79
Jimmy Collazos <acido69@gmail.com>
2013-05-20 18:11:01 +0200

    bug/IDAS-10300 add .m2m-btn.accepted style for button

--- components/application_boxmessage.js
+++ components/application_boxmessage.js
@@ -10,24 +10,24 @@ define(
         title: $('<p class="box-message-title"></p>'),
         message: $('<p></p>'),
         btndiv: $('<div><button name="close"></button></div>'),
-        btnsdiv: $('<div><button name="ok"></button><button name="cancel"></button></div>')
+        btnsdiv: $('<div><button name="cancel" class="m2m-btn"></button><button name="ok" class="m2m-btn accept"></button></div>')
     };
-    
+
     var BUTTON_TEXT_CLOSE = 'Accept';
     var BUTTON_TEXT_OK = 'Accept';
     var BUTTON_TEXT_CANCEL = 'Cancel';
-    var btnClose; 
+    var btnClose;
     var btnOk;
     var btnCancel;
 
     var BoxMessage = {
-        
+
       close: function() {
         el.box.hide();
         el.bg.hide();
         el.title.hide();
       },
-      
+
       open: function(opt) {
          if(opt.confirmModal){
             btnOk.text(opt.buttonTextOk || BUTTON_TEXT_OK);
@@ -40,34 +40,34 @@ define(
             el.btnsdiv.hide();
             el.btndiv.show();
         }
-        
+
         if (opt.title) {
           el.title.html(opt.title);
           el.title.show();
         }
-        
+
         if (opt.message) {
           el.message.html(opt.message);
           el.box.show();
           el.bg.show();
         }
-        
+
       }
     };
 
     function init() {
-      
+
       btnClose = el.btndiv.find('[name="close"]');
       btnOk = el.btnsdiv.find('[name="ok"]');
       btnCancel = el.btnsdiv.find('[name="cancel"]');
-      
+
       el.bg.hide();
       el.box.hide();
       el.title.hide();
 
       $('body').append(el.bg, el.box);
       el.box.append(el.title, el.message, el.btndiv, el.btnsdiv);
-      
+
       btnClose.on('click', BoxMessage.close);
       btnCancel.on('click', BoxMessage.close);
     }

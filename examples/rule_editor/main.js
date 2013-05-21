83c7849a9111c827da3cc25c692129adfd467cd5
davidg <davidg@tid.es>
2013-05-21 11:58:39 +0200

    Removed process card from main. Card toolbox uses card_data too

--- examples/rule_editor/main.js
+++ examples/rule_editor/main.js
@@ -34,23 +34,21 @@ define(
                     },
                     actions: {
                         label: 'Actions',
-                        cards: [{
-                            cssClass: 'm2m-card-action m2m-card-send-email',
-                            header: 'Send Email',
-                            component: 'SendEmail',
-                            tokens: ['device_latitude', 'device_longitude', 'measure.value', 'device.asset.name']
-                        }]
+                        cards: []
                     }
                 };
 
-                $http.get('cards.json').success(function(data) {
-                    var cards = processCards(data.data);
-                    $scope.cards.conditions.cards = cards;
+                $http.get('actions.json').success(function(data) {
+                    $scope.cards.actions.cards = data.data;
                 });
 
-                //$http.get('rule.json').success(function(data) {
-                //    $scope.ruleData = data.data[0];
-                //});
+                $http.get('cards.json').success(function(data) {
+                    $scope.cards.conditions.cards = data.data;
+                });
+
+                $http.get('rule.json').success(function(data) {
+                    $scope.ruleData = data.data[0];
+                });
 
                 $scope.$watch('ruleData', function() {
                     console.log('RuleData change', $scope.ruleData);
@@ -61,72 +59,5 @@ define(
         angular.bootstrap(document, ['testApp']);
 
     });
-
-    function processCards(cardsData) {
-        var cards = [];
-        $.each(cardsData, function(i, cardData) {
-            var card = $.extend({}, cardData.configData);
-            card.header = cardData.sensorData.measureName;
-            cards.push(card);
-
-            if (cardData.sensorData.phenomenon ===
-                "urn:x-ogc:def:phenomenon:IDAS:1.0:angle") {
-                card.front = {
-                        items: [{
-                            component: 'AngleWidget'
-                        }]
-                    };
-                card.back = {
-                        items: [{
-                            component: 'Slider'
-                        }]
-                    };
-            } else if (cardData.sensorData.phenomenon ===
-                "urn:x-ogc:def:phenomenon:IDAS:1.0:electricPotential") {
-                card.front = {
-                    items: [{
-                        component: 'Battery'
-                    }]
-                };
-                card.back = {
-                        items: [{
-                            component: 'Slider'
-                        }]
-                    };
-            } else if (cardData.sensorData.dataType === "Boolean") {
-                card.front = {
-                    items: [{
-                        component: 'CardFrontBinary'
-                    }]
-                };
-                card.back = {
-                    items: [{
-                        component: 'Dropdown',
-                        defaultValue: 'false',
-                        options: [{
-                            label: 'True', 
-                            value: 'true'
-                        }, {
-                            label: 'False', 
-                            value: 'false'
-                        }]
-                    }]
-                };
-            } else {
-                card.front = {
-                    items: [{
-                        component: 'CardFrontText'
-                    }]
-                };
-                card.back = {
-                        items: [{
-                            component: 'CardBackText'
-                        }]
-                    };
-            }
-        });
-        return cards;
-    }
-
   }
 );

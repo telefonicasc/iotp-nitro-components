d02f324b2c11a1ed491e30b35f5637a18e9e2572
davidg <davidg@tid.es>
2013-05-22 11:17:48 +0200

    Fixed some styles for the rest of the cards

--- examples/rule_editor/main.js
+++ examples/rule_editor/main.js
@@ -46,9 +46,9 @@ define(
                     $scope.cards.conditions.cards = data.data;
                 });
 
-                $http.get('rule.json').success(function(data) {
-                    $scope.ruleData = data.data[0];
-                });
+                //$http.get('rule.json').success(function(data) {
+                //    $scope.ruleData = data.data[0];
+                //});
 
                 $scope.$watch('ruleData', function() {
                     console.log('RuleData change', $scope.ruleData);

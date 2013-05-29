800d2e58e0eab841b005788f772343c9367cc32c
Manuel J. Romero <mjromper@gmail.com>
2013-05-29 11:27:46 +0100

    Fixed perdiod selection

--- components/chart/column_chart.js
+++ components/chart/column_chart.js
@@ -11,8 +11,8 @@ define(
         function ColumnChart() {
 
             this.defaultAttrs({
-                step: 7,
-                opacity: 0.02
+                opacity: 0.02,
+                paddingColumn: 3
             });
 
             this.after('initialize', function() {
@@ -20,16 +20,20 @@ define(
                 var x = d3.time.scale().range([0, this.width]),
                     y = d3.scale.linear().range([this.height, 0]),
                     data = this.$node.data('value') || this.attr.value || [],
+                    columns = 0,
+                    columnsData = [],
                     context = d3.select(this.node).append('svg');
+
                 context.attr('class', 'chart ' + this.attr.cssClass);
                 var subPanelgroup = context.append('g');
                 var carousel = [];
 
                 this.updateChart = function() {
-                    var _width = this.width,
-                        _height = this.height,
+
+                    var columnWidth = this.width/(columns)-this.attr.paddingColumn,
                         _attr = this.attr;
-                    context.attr('width', _width);
+
+                    context.attr('width', this.width);
 
                     carousel.length = 0;
                     subPanelgroup.remove();
@@ -42,8 +46,8 @@ define(
                     .style('opacity', function(d, i){
                         return (i%2 === 0)? _attr.opacity: 0;
                     })
-                    .attr('width', _width/(this.attr.step-2))
-                    .attr('height', _height);
+                    .attr('width', columnWidth)
+                    .attr('height', this.height);
 
                     this.bars.exit().remove();
                     var _items = this.attr.items;
@@ -56,26 +60,29 @@ define(
                         .enter().append('foreignObject')
                         .attr('class', 'cell-barchart-subpanel')
                         .attr('x', function(d) {
-                            
                             return x(d.date);
                         })
-                        .attr('width', _width/(this.attr.step-2)+3)
-                        .attr('height', _height);
+                        .attr('width', columnWidth)
+                        .attr('height', this.height);
                     }
 
                     if (_attr.items){
-                       $('.cell-barchart-subpanel').each(function(i, panel){
-                            ComponentManager.get(_attr.items[0].component).attachTo(panel, $.extend({}, _attr.items[0].text, _attr.items[0].chart));
-                            
-                            $(panel).trigger('valueChange', { text1:data[i].text1, text2:data[i].text2, values: data[i].values } );
-                        }); 
+                        $('.cell-barchart-subpanel').each(function(i, panel){
+                            var attr = $.extend({}, _attr.items[0].text, _attr.items[0].chart);
+                            ComponentManager.get(_attr.items[0].component).attachTo(panel, attr);
+                            $(panel).trigger('valueChange',
+                                { text1:data[i].text1, text2:data[i].text2, values: data[i].values } );
+                        });
                     }
-                    
+
                 };
 
                 this.on('resize', function(e, chartSize) {
                     this.width = chartSize.width;
                     this.height = chartSize.height;
+                    $('.cell-barchart-subpanel').each(function(i, panel){
+                            $(panel).trigger('resize' );
+                    });
                     x.range([0, this.width]);
                     y.range([this.height, 0]);
                     this.updateChart();
@@ -83,39 +90,54 @@ define(
                 });
 
                 this.on('valueChange', function(e, options) {
+                    e.stopPropagation();
+
+                    if (!options.value.fixRange){ return; }
 
                     var valueField = this.attr.model;
-                    this.attr.value = $.map(options.value[valueField], function(val, i) {
-                        if (val.date > options.range[0] &&
-                            val.date <= options.range[1]) {
-                            val.date = d3.time.day.round(new Date(val.date));
-                            return val;
-                        }
-                    });
+                    this.attr.value = options.value[valueField];
 
-                    if (!data || data.length === 0){
-                        var step = this.attr.step;
-                        data = [];
-                        $.each(this.attr.value, function(i, val){
-                            if (i % step === 0) {
-                                data.push({date:val.date, 
-                                    values: [
-                                        { name:'+', value: Math.floor(Math.random()*(91)+1)  }, 
-                                        { name:'-', value: Math.floor(Math.random()*(91)+1) } 
-                                    ],
-                                    text1: Math.floor(Math.random()*(91)+1)+'%',
-                                    text2: Math.floor(Math.random()*(1001)+1)
-                                });
-                            }
-                        }); 
+                    var daysTick = (options.value.fixRange === 35) ? 7 : 1;
+
+                    if (columns !== options.value.fixRange/daysTick){
+                        columnsData = getData(this.attr.value, daysTick);
                     }
 
-
+                    columns = options.value.fixRange/daysTick;
+                    data = [];
+                    $.each(this.attr.value, function(i, val){
+                        if (i % daysTick === 0) {
+                            data.push({date:val.date,
+                                values: columnsData[i/daysTick]['values'],
+                                text1: columnsData[i/daysTick]['text1'],
+                                text2: columnsData[i/daysTick]['text2']
+                            });
+                        }
+                    });
                     x.domain(options.range);
                     this.updateChart();
 
-                    e.stopPropagation();
                 });
+
+                function getData(values, daysTick){
+                    var list = [],
+                        val1 = 0,
+                        val2 = 0;
+                    $.each(values, function(i, item){
+                        val1 += item.value;
+                        val2 += item.value2;
+                        if (i % daysTick === 0) {
+                            var data = { values:[], text1: Math.floor(Math.random()*(91)+1)+'%', text2:  Math.floor(Math.random()*(1001)+1) };
+                            data['values'].push({ name:'+', value: val1  });
+                            data['values'].push({ name:'-', value: val2 });
+                            list.push(data);
+                            val1 = 0;
+                            val2 = 0;
+                        }
+
+                    });
+                    return list;
+                }
 
             });
         }

c58043816bb772d0cd27842a25d4dd33b13b765e
Jimmy Collazos <acido69@gmail.com>
2013-05-29 12:31:27 +0200

    bug/IDAS-10698 VR - add default conditionList

--- components/card/card.js
+++ components/card/card.js
@@ -36,11 +36,11 @@ define(
                     'parameterValue': null,
                     'not': false,
                     'operator': null
-                }
+                },
+                defaultValue : ''
             });
 
             this.after('initialize', function() {
-
                 var elementId = this.attr.id || _getNexId(),
                     dragCheck = false, clickCheck = false;
 
@@ -55,7 +55,6 @@ define(
                     this.attr.front.header = this.attr.header;
                     this.attr.back.header = this.attr.header;
                 }
-
                 CardSide.attachTo(this.$front, this.attr.front);
                 CardSide.attachTo(this.$back, this.attr.back);
 
@@ -96,7 +95,7 @@ define(
                     if(this.attr.conditionList.length){
                         condition = this.attr.conditionList[0];
                     }else{
-                        condition = this.attr.defaultCondition;
+                        condition = $.extend({}, this.attr.defaultCondition);
                     }
 
                     this.on('conditionOperatorChange', function(e,o){
@@ -109,12 +108,14 @@ define(
                         condition.parameterValue = o.value;
                         this.$node.data('conditionList', [condition]);
                     });
-                    if(condition.parameterValue !== null && condition.operator !== null){
-                        this.$node.data('conditionList', [condition]);
+
+                    if(condition.parameterValue !== null ){
                         this.$node.find('.body > *' ).trigger('valueChange', { value: condition.parameterValue });
                     }else{
-                        this.$node.data('conditionList', []);
+                        condition.parameterValue = this.attr.defaultValue;
                     }
+                    this.$node.data('conditionList', [condition]);
+
                 }
             });
 
--- components/card/card_data.js
+++ components/card/card_data.js
@@ -24,6 +24,7 @@ function() {
                     component: component.SLIDER
                 }]
             };
+            card.defaultValue = '0';
             return card;
         },
         'battery': function(card){
@@ -37,6 +38,7 @@ function() {
                     component: component.SLIDER
                 }]
             };
+            card.defaultValue = '0';
             return card;
         },
         'binary': function(card) {
@@ -58,6 +60,7 @@ function() {
                     }]
                 }]
             };
+            card.defaultValue = 'false';
             return card;
         },
         'text': function(card) {

70afea1049f2a697ff09feb699af2d05a606f91d
Jimmy Collazos <acido69@gmail.com>
2013-06-03 15:34:15 +0200

    bug/IDAS-10706 Boxmessage - add parameter for set name of buttons

--- components/application_boxmessage.js
+++ components/application_boxmessage.js
@@ -1,82 +1,89 @@
 define(
-  [
-    'components/component_manager'
-  ],
+    [
+        'components/component_manager'
+    ],
 
-  function() {
-    var el = {
-        bg: $('<div class="box-message-bg"></div>'),
-        box: $('<div class="box-message-msg"></div>'),
-        title: $('<p class="box-message-title"></p>'),
-        message: $('<p class="box-message-content"></p>'),
-        btndiv: $('<div class="box-message-buttons"><button name="close" class="m2m-btn accept"></button></div>'),
-        btnsdiv: $('<div class="box-message-buttons"><button name="cancel" class="m2m-btn"></button><button name="ok" class="m2m-btn accept"></button></div>')
-    };
+    function() {
+        var el = {
+            bg: $('<div class="box-message-bg"></div>'),
+            box: $('<div class="box-message-msg"></div>'),
+            title: $('<p class="box-message-title"></p>'),
+            message: $('<p class="box-message-content"></p>'),
+            btndiv: $('<div class="box-message-buttons"><button name="close" class="m2m-btn accept"></button></div>'),
+            btnsdiv: $('<div class="box-message-buttons"><button name="cancel" class="m2m-btn"></button><button name="ok" class="m2m-btn accept"></button></div>')
+        };
 
-    var BUTTON_TEXT_CLOSE = 'Accept';
-    var BUTTON_TEXT_OK = 'Accept';
-    var BUTTON_TEXT_CANCEL = 'Cancel';
-    var btnClose;
-    var btnOk;
-    var btnCancel;
+        var btnClose;
+        var btnOk;
+        var btnCancel;
+        var defaultOption = {
+            title:'',
+            message:'',
+            confirmModal:false,
+            okCallback:false,
+            button:{
+                'accept':{label:'Accept', callback:function(){}},
+                'cancel':{label:'Cancel', callback:function(){}},
+                'close':{label:'Accept', callback:function(){}}
+            }
+        };
 
-    var BoxMessage = {
+        var BoxMessage = {
+            close: function() {
+                el.box.hide();
+                el.bg.hide();
+                el.title.hide();
+            },
+            open: function(opt) {
+                var option = $.extend(true, {}, defaultOption, opt);
+                var focusEl = null;
+                if (option.confirmModal) {
+                    btnOk.text(option.button.accept.label);
+                    btnCancel.text(option.button.cancel.label);
+                    el.btnsdiv.show();
+                    el.btndiv.hide();
+                    btnOk.on('click', option.okCallback || option.button.accept.callback);
+                    btnCancel.on('click', option.button.close.callback);
+                    focusEl = btnOk;
+                } else {
+                    btnClose.on('click', option.button.close.callback);
+                    btnClose.text(option.button.close.label);
+                    el.btnsdiv.hide();
+                    el.btndiv.show();
+                    focusEl = btnClose;
+                }
 
-      close: function() {
-        el.box.hide();
-        el.bg.hide();
-        el.title.hide();
-      },
+                if (option.title) {
+                    el.title.html(option.title);
+                    el.title.show();
+                }
 
-      open: function(opt) {
-	 var focusEl = null;
-         if (opt.confirmModal) {
-            btnOk.text(opt.buttonTextOk || BUTTON_TEXT_OK);
-            btnCancel.text(opt.buttonTextCancel || BUTTON_TEXT_CANCEL);
-            el.btnsdiv.show();
-            el.btndiv.hide();
-            btnOk.on('click', opt.okCallback);
-	    focusEl = btnOk;
-        } else {
-            btnClose.text(opt.buttonText || BUTTON_TEXT_CLOSE);
-            el.btnsdiv.hide();
-            el.btndiv.show();
-	    focusEl = btnClose;
+                if (option.message) {
+                    el.message.html(option.message);
+                    el.box.show();
+                    el.bg.show();
+                    focusEl.focus();
+                }
+            }
+        };
+
+        function init() {
+            btnClose = el.btndiv.find('[name="close"]');
+            btnOk = el.btnsdiv.find('[name="ok"]');
+            btnCancel = el.btnsdiv.find('[name="cancel"]');
+
+            el.bg.hide();
+            el.box.hide();
+            el.title.hide();
+
+            $('body').append(el.bg, el.box);
+            el.box.append(el.title, el.message, el.btndiv, el.btnsdiv);
+
+            btnClose.on('click', BoxMessage.close);
+            btnCancel.on('click', BoxMessage.close);
         }
 
-        if (opt.title) {
-          el.title.html(opt.title);
-          el.title.show();
-        }
-
-        if (opt.message) {
-          el.message.html(opt.message);
-          el.box.show();
-          el.bg.show();
-	  focusEl.focus();
-        }
-
-      }
-    };
-
-    function init() {
-
-      btnClose = el.btndiv.find('[name="close"]');
-      btnOk = el.btnsdiv.find('[name="ok"]');
-      btnCancel = el.btnsdiv.find('[name="cancel"]');
-
-      el.bg.hide();
-      el.box.hide();
-      el.title.hide();
-
-      $('body').append(el.bg, el.box);
-      el.box.append(el.title, el.message, el.btndiv, el.btnsdiv);
-
-      btnClose.on('click', BoxMessage.close);
-      btnCancel.on('click', BoxMessage.close);
+        init();
+        window.BoxMessage = BoxMessage;
     }
-
-    init();
-    window.BoxMessage = BoxMessage;
-  }
 );

2500ca9f225c75d4d42f14d0466c1d8d454ccabc
davidg <davidg@tid.es>
2013-06-13 10:47:53 +0200

    Created repeat container

---
+++ components/repeat_container.js
@@ -0,0 +1,47 @@
+define(
+    [
+        'components/component_manager',
+        'components/container',
+        'components/mixin/data_binding'
+    ],
+
+    function(ComponentManager, Container, DataBinding) {
+
+        return ComponentManager.create('RepeatContainer',
+            RepeatContainer, DataBinding);
+
+        function RepeatContainer() {
+
+            this.defaultAttrs({
+                item: { component: 'container' }
+            });
+
+            this.updateContent = function(dataItems) {
+                this.$node.empty();
+                $.each(dataItems, $.proxy(function(i, item) {
+                    var cmp = ComponentManager.get(this.attr.item.component),
+                        itemNode = $('<div>')
+                            .addClass('repeat-container-item'); 
+                    
+                    cmp.attachTo(itemNode, $.extend({
+                        model: (function(index) {
+                            return function(data) {
+                                return data ? data[i] : null;
+                            };
+                        })(i)
+                    }, this.attr.item));
+                    this.$node.append(itemNode);
+                }, this));
+            };
+    
+            this.after('initialize', function() {
+                this.$node.addClass('repeat-container');
+                this.on('valueChange', function(e, o) {
+                    if (o.value && o.value.length) {
+                        this.updateContent(o.value); 
+                    }
+                });
+            });
+        }
+    }
+);

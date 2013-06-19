c683e598f893fde8480a335ae8fe4dd4751b6f0a
Manuel J. Romero <mjromper@gmail.com>
2013-06-19 18:06:00 +0100

    Improving group bar chart

--- components/chart/group_bar_chart.js
+++ components/chart/group_bar_chart.js
@@ -5,14 +5,12 @@ define(
 
     function(ComponentManager, CellBarchartSubpanel) {
 
-        return ComponentManager.create('groupBarChart',
-            GroupBarChart);
+        return ComponentManager.create('groupBarChart', GroupBarChart);
 
         function GroupBarChart(){
 
             this.defaultAttrs({
                 grid: true,
-                data: {},
                 incremental: true,
                 colors: ['#000000']
             });
@@ -26,129 +24,124 @@ define(
                     context = d3.select(this.node).append('svg')
                         .attr('class', 'group-barchart')
                         .attr('width', this.width)
-                        .attr('height', (this.height));
+                        .attr('height', this.height),
+                    keys = [];    
 
                 var axisX, axisY, xAxis, yAxis = null;
                 if (this.attr.grid){
                     axisX = context.append('g').attr('class', 'axis_x');
                     axisY = context.append('g').attr('class', 'axis_y');
                     xAxis = d3.svg.axis().scale(x0).orient('bottom');
-                    yAxis = d3.svg.axis().scale(y).orient('right').tickFormat(d3.format('1'));
+                    yAxis = d3.svg.axis().scale(y).orient('right');
                 }
 
-                this.value = this.attr.data,
-                this.keys = getObjKeys(this.value);
+                this.values = null, 
                 this.maxValue = 100;
 
-                var barGroups = context.selectAll('.barGroup').data(this.keys),
-                backgroundGroups = context.append('svg'),
-                subPanelgroup = context.append('svg');
+                var barGroups = context.selectAll('.barGroup');
+                var backgroundGroups = context.append('g').attr('class','backgroundGroups');
+                var carouselGroup = context.append('g').attr('class', 'carouselGroup');
 
                 if (this.attr.tooltip){
                     this.tooltip = $('<div>').addClass('tooltip').appendTo($('body'));
                 }
 
+
                 this.updateChart = function() {
 
                     context.attr('height', (this.height+130));
 
-                    barGroups.remove();
-                    backgroundGroups.remove();
-                    subPanelgroup.remove();
-
-                    this.keys = getObjKeys(this.value);
-
-                    //Grouping data by 'key'
-                    var values = [], barIndexList = [];
-                    var _data = this.value;
-                    $.each(this.keys, function(j, k){
-                        var barsGroup = [] ;
-                        barIndexList = [];
-                        $.each(_data[k], function(i, val){
-                            barIndexList.push(i);
-                            barsGroup.push( {index: i, value: val} );
-                        });
-                        values.push(barsGroup);
-                    });
-
-                    x0.domain(this.keys.map(function(key) {
-                        return key;
-                    }));
-                    x1.domain(barIndexList).rangeRoundBands([0, x0.rangeBand()]);
-                    /*y.domain([0, d3.max(values, function(d) {
-                        return d3.max(d, function(d) {
-                            return d.value;
-                        });
-                    
-                    })]);*/
-                    y.domain([0, this.maxValue]);
                     if (axisX && axisY) {
                         axisX.attr('transform', 'translate(0, '+this.height+')').call(xAxis);
                         axisY.attr('transform', 'translate('+this.width+', 0)').call(yAxis);
                     }
 
-                    //Background rectangles
-                    backgroundGroups = context.append('g');
-                    backgroundGroups.selectAll('.bg_group')
-                    .data(this.keys)
-                    .enter().append('rect')
-                    .attr('class', function(key, i){
-                        return (i%2 === 0)? 'bg_group odd' : 'bg_group';
-                    })
-                    .attr('x', function(key) {
-                        return  x0(key)-x0(0)/2;
-                    })
-                    .attr('width', this.width/this.keys.length )
-                    .attr('height', this.height);
-
-                    var keys = this.keys;
-                    subPanelgroup = context.append('g');
-                    subPanelgroup.attr('transform', 'translate(0, '+(this.height+30)+')');
-                    subPanelgroup.selectAll('.foreignObject')
-                    .data(this.keys)
-                    .enter().append('foreignObject')
-                    .attr('class', function(key, i){
-                        return 'cell-barchart-subpanel';
-                    })
-                    .attr('x', function(key) {
-                        return  x0(key)-x0(0)/2+3;
-                    })
-                    .attr('width', this.width/keys.length-6)
-                    .attr('height', 100);
-
-                    //Attach subpanels
-                    $('.cell-barchart-subpanel').each(function(i, panel){
-                        ComponentManager.get('carouselPanel').attachTo(panel);
-                    });
-
-                    //Bar groups
-                    barGroups = context.selectAll('.group').data(values);
-                    barGroups.enter().append('g')
-                        .attr('fill', function(key, i) {
-                            return colors[(i%colors.length)];
-                        })
-                        .attr('class', 'group')
-                        .attr('transform', function(d, i) {
-                            return 'translate(' + x0(keys[i]) + ',0)';
+                    x0.domain(keys.map(function(key) {
+                        return key;
+                    }));
+                    var rangeGroup = d3.range(this.values[0].length);
+                    x1.domain(rangeGroup).rangeRoundBands([0, x0.rangeBand()]);
+                    var maxValuePeriod = d3.max(this.values, function(d) {
+                        return d3.max(d, function(d) {
+                            return d;
                         });
+                    
+                    });
+                    //y.domain([0, maxValuePeriod]);
+                    y.domain([0, this.maxValue]);
+        
+
+                    if (this.newGroups){
+                        //********** Background rectangles
+                        var bgGroups = backgroundGroups.selectAll('.bg_group').data(keys);
+                        bgGroups.enter().append('rect')
+                        .attr('class', function(key, i){
+                            return (i%2 === 0)? 'bg_group odd' : 'bg_group';
+                        })
+                        .attr('x', function(key) {
+                            return  x0(key)-x0(0)/2;
+                        })
+                        .attr('width', this.width/keys.length )
+                        .attr('height', this.height);
+                        bgGroups.exit().remove();
+
+                        //************ Carousel panels               
+                        carouselGroup.attr('transform', 'translate(0, '+(this.height+30)+')');
+                        
+                        this.carousel = carouselGroup.selectAll('.cell-barchart-subpanel')
+                        .data(keys).enter().append('foreignObject')
+                        .attr('class', function(key, i){
+                            return 'cell-barchart-subpanel';
+                        })
+                        .attr('height', 100)
+                        .attr('x', function(key) {
+                            return  x0(key)-x0(0)/2+3;
+                        })
+                        .attr('width', this.width/keys.length-6);
+                        
+
+                        //Attach carousel subpanels
+                        $('.cell-barchart-subpanel').each(function(i, panel){
+                            ComponentManager.get('carouselPanel').attachTo(panel);
+                        });
+                    }
+
+                   
+                    
+
+
+                    //************ Bar groups
+                    barGroups.remove();
+                    barGroups = context.selectAll('.group').data(this.values);
+                    barGroups.enter().append('g')
+                    .attr('fill', function(key, i) {
+                        return colors[(i%colors.length)];
+                    })
+                    .attr('class', function(key){
+                        return 'group';
+                    })
+                    .attr('transform', function(d, i) {
+                        return 'translate(' + x0(keys[i]) + ',0)';
+                    });
+                    barGroups.exit().remove();
+
 
                     //Bars
                     var self = this;
                     var bars = barGroups.selectAll('.chartbar')
-                    .data(function(d) {
-                        return d;
-                    });
+                    .data(function(d) { return d; });
+
                     bars.enter().append('rect')
                     .attr('class', 'chartbar')
                     .attr('width', x1.rangeBand()-1)
-                    .attr('x', function(d,i) {
-                        return x1(d.index);
+                    .attr('x', function(d, i) {
+                        return x1(i);
                     })
                     .attr('y', function(d) {
-                        return y(d.value);
+                        return y(d);
                     })
                     .attr('height', function(d) {
-                        return self.height - y(d.value);
+                        return self.height - y(d);
                     });
 
                     if (this.attr.tooltip){
@@ -166,12 +159,13 @@ define(
                 };
 
                 this.updateSubpanel = function(){
-                    var keys = getObjKeys(this.value);
+                    
                     var self = this;
                     
                     //Propagate valueChange to each subpanel
                     $('.cell-barchart-subpanel').each(function(i, panel){
-                        var count = d3.max(self.value[keys[i]]);
+                        var lastIndex = self.values[0].length-1;
+                        var count = self.values[i][lastIndex];
 
                         var val = {
                             text1:  round(count/self.totalCount*100)+' %',
@@ -184,9 +178,13 @@ define(
 
                 };
 
+                this.transition = function(){
+
+                };
+
                 this.showTooltip = function(rect, d) {
                     var pos = $(rect).offset();
-                    this.tooltip.html('<div class="value">'+((this.modelData.unit)? this.modelData.unit: '')+' '+round(d.value)+'</div><div class="caption">('+this.attr.daysBar+' days/bar)</div>');
+                    this.tooltip.html('<div class="value">'+((this.modelData.unit)? this.modelData.unit: '')+' '+round(d)+'</div><div class="caption">('+this.attr.daysBar+' days/bar)</div>');
                     this.tooltip.css({
                         top: pos.top,
                         left: pos.left + x1.rangeBand()/3
@@ -199,33 +197,57 @@ define(
 				};
 
 				this.on('resize', function(e, chartSize) {
-                    e.stopPropagation();
-
+                   
 					this.width = chartSize.width;
 					this.height = chartSize.height;
+
+                    //Update axe ranges
 					x0.rangeRoundBands([0, this.width], 0.1);
 					y.range([this.height, 0]);
-					this.updateChart();
-                    this.updateSubpanel();
+                    
+					
+                    //Update
+                    if (this.values){
+                        
+                        //carouselGroup.selectAll('.cell-barchart-subpanel').remove();
+                        backgroundGroups.selectAll('.bg_group').remove();
+                        this.newGroups = true;
+                        
+                        this.updateChart();
+
+                        this.updateSubpanel();
+                    }    
+                    e.stopPropagation();
+     
 					
 				});
 
 				this.on('valueChange', function(e, options) {
-                    e.stopPropagation();
+                    e.stopPropagation();     
 
-                    if (!this.attr.model){
-                        console.log('No model!');
-                        return;
-                    }
+                    if (!this.attr.model){ return; }
+                    
                     var fixRange = options.value.fixRange;
                     var roundDate = d3.time.day.round(new Date(options.range[0])).getTime();
                     
                     this.modelData = options.value[this.attr.aggregation+this.attr.model][fixRange];
                     this.maxValue = this.modelData.maxValue + 5;
                     this.attr.daysBar = (fixRange === 7)? 1 : 7;   
-                    this.value = this.modelData.values[roundDate];
-                    this.totalCount = this.modelData.totalCount[roundDate]
+                    var rawValues = this.modelData.values[roundDate];
+                    this.totalCount = this.modelData.totalCount[roundDate];
                     this.options = options;
+
+                    var newKeys = getObjKeys(rawValues);
+                    this.newGroups = newKeys.join(',') !== keys.join(',');
+                    if (this.newGroups) {
+                        keys = newKeys;
+                    }
+
+                    //Aggregate data by 'group'
+                    this.values = [];
+                    for (group in rawValues){
+                        this.values.push(rawValues[group]);
+                    }  
 
                     this.updateChart();
                     this.updateSubpanel();

7adb05a5438d8f10a59cdeffba553b6b7ec365ed
Manuel J. Romero <mjromper@gmail.com>
2013-07-01 18:08:25 +0100

    Created tooltip mixin. Improved performance area stacked chart. Creation and update attributes separately. Added some animations.

--- components/chart/area_stacked_chart.js
+++ components/chart/area_stacked_chart.js
@@ -1,12 +1,13 @@
 define(
     [
-        'components/component_manager'
+        'components/component_manager',
+        'components/chart/mixin/tooltip'
     ],
 
-    function(ComponentManager) {
+    function(ComponentManager, Tooltip) {
 
         return ComponentManager.create('areaStackedChart',
-            AreaStackedChart);
+            AreaStackedChart, Tooltip);
 
         function AreaStackedChart() {
 
@@ -14,7 +15,8 @@ define(
                 colorArea: ['#ff0000'],
                 colorLine: ['#00ff00'],
                 fillOpacity: 0.85,
-                subModelsSufix: []
+                subModelsSufix: [],
+                autoscale: false
             });
 
             this.after('initialize', function() {
@@ -23,13 +25,14 @@ define(
                     y = d3.scale.linear().range([this.height, 0]),
                     data = this.$node.data('value') || [],
                     context = d3.select(this.node);
+                var svg = context.append('g');
+    
 
                 context.attr('class', 'chart stacked');
 
-                if (this.attr.tooltip) {
-                    this.tooltip = $('<div>').addClass('tooltip')
-                        .appendTo($('body'));
-                }
+                this.maxRangeValue = 0;
+                this.initialized = null;
+                this.range0 = 0;
 
                 var stack = d3.layout.stack()
                   .offset('zero')
@@ -48,20 +51,12 @@ define(
                   .x(function(d) { return x(d.date); })
                   .y(function(d) { return y(d.y0 + d.value); });
 
-                var svg = context.select('svg').append('g');
 
-                this.updateChart = function() {
-
-                    context.select('svg').remove();
-                    svg = context.append('svg').append('g');
-
-                    var attrib = this.attr;
-
-                    var areaColor = this.attr.colorArea;
-
-                    if (this.attr.colorPattern){
-                      areaColor = [];
-                      $.each(this.attr.colorPattern, function( i, pattern ) {
+                var areaColor = this.attr.colorArea;
+                var self = this;
+                if (this.attr.colorPattern){
+                    areaColor = [];
+                    $.each(this.attr.colorPattern, function( i, pattern ) {
                         var defs = svg.append('svg:defs');
                         defs.append('svg:pattern')
                           .attr('id', 'pattern_'+pattern)
@@ -76,64 +71,84 @@ define(
                           .attr('height', 20);
 
                         areaColor.push('url(#pattern_'+pattern+')');
-                      });           
-                    }
+                    });           
+                }
+
+                this.createChart = function(){
+                                  
+                  var attrib = this.attr; 
+                  var self = this;       
+                  if (data && data.length > 0){
+
+                      var layers = stack(data);
+                      svg.selectAll('.layer')
+                        .data(layers)
+                        .enter().append('svg:path')
+                        .attr('class', function(d, i){
+                            return 'layer '+attrib.cssClass[i];
+                        })
+                        .style('fill', function(d, i){
+                            return areaColor[i];
+                        })
+                        .style('fill-opacity', attrib.fillOpacity)
+                        .attr('d', function(d) { return area(d.values); });
+
+                      svg.selectAll('.line')
+                        .data(layers)
+                        .enter().append('svg:path')
+                        .attr('class', function(d){
+                          return 'line '+d.key;
+                        })
+                        .style('stroke', function(d, i){
+                          return attrib.colorLine[i];
+                        })
+                        
+                        .attr('d', function(d) { return line(d.values); });; 
+                  }     
+
+                };
+
+                this.updateChart = function() {
+
+                    var attrib = this.attr;                    
 
                     svg.attr('width', this.width).attr('height', this.height);
-
+                    var self = this;
                     if (data && data.length > 0){
 
                         var layers = stack(data);
-                        var self = this;
+                        
+                        var areas = svg.selectAll('.layer').data(layers);
+                        var lines = svg.selectAll('.line').data(layers);
 
-                        svg.selectAll('.layer')
-                          .data(layers)
-                          .enter().append('svg:path')
-                          .attr('class', function(d, i){
-                              'layer'+attrib.cssClass[i];
-                          })
-                          .attr('d', function(d) { return area(d.values); })
-                          .style('fill', function(d, i){
-                              return areaColor[i];
-                          })
-                          .style('fill-opacity', attrib.fillOpacity);
+                        if (this.anim){
+                            lines.transition().ease('linear').duration(200).attr('d', function(d) { return line(d.values); });
+                            areas.transition().ease('linear').duration(200).attr('d', function(d) { return area(d.values); }); 
+                        }else{
+                            lines.attr('d', function(d) { return line(d.values); });
+                            areas.attr('d', function(d) { return area(d.values); }); 
+                        }
                         
-                        svg.selectAll('.line')
-                          .data(layers)
-                          .enter().append('svg:path')
-                          .attr('d', function(d) { return line(d.values); })
-                          .style('stroke', function(d, i){
-                            return attrib.colorLine[i];
-                          })
-                          .attr('class', function(d){
-                            return 'line '+d.key;
-                          });
-                        
-                        if (attrib.tooltip) {
+                        if (this.attr.tooltip) {
 
                             $.each(data, function(i, val){
-
+                                svg.selectAll('.hoverCircle').remove();
                                 var hoverCircle = null;
-
                                 hoverCircle = svg.selectAll('.hoverCircle '+val.key)
                                     .data(val.values);
-
                                 hoverCircle.enter().append('circle')
                                     .attr('r', 6)
                                     .attr('opacity', 0)
-                                    .attr('class', 'hoverCircle '+val.key)
-                                    .on('mouseover', function(d) {
-                                        d3.select(this).attr('opacity', 1);
-                                        self.showTooltip(this, d);
-                                    })
-                                    .on('mouseout', function(d) {
-                                        d3.select(this).attr('opacity', 0);
-                                        self.hideTooltip();
+                                    .attr('class', function(d){
+                                      return 'hoverCircle '+val.key;
                                     })
                                     .attr('transform', function (d, i){
                                         return 'translate('+x(d.date)+','+y(d.y0 + d.value)+')';
+                                    })
+                                    .on('mouseover', function(d) {
+                                        self.showTooltip(this, d);
                                     });
-
+                                   
                                 hoverCircle.exit().remove();
                      
                             });  
@@ -154,27 +169,49 @@ define(
                 this.on('valueChange', function(e, options) {
                     e.stopPropagation();
 
+                    if (!options.range || this.range0 === options.range[0].getTime()){
+                      return;
+                    }
+                    this.range0 = options.range[0].getTime();
+
                     var valueField = this.attr.model;
                     data = [];
-
+                    var maxValue = 0;
                     $.each(this.attr.subModelsSufix, function (i, submodel){
-                      var values = $.map(options.value[valueField+submodel], function(val, i) {
-                          if (val.date >= options.range[0] &&
-                              val.date <= options.range[1]) {
-                              return val;
-                          }
+                        var values = $.map(options.value[valueField+submodel], function(val, i) {
+                            if (val.date >= options.range[0] &&
+                                val.date <= options.range[1]) {
+                                if ((val.value+val.y0) > maxValue) {
+                                  maxValue = (val.value+val.y0);
+                                }
+                                return val;
+                            }
+                        });
+
+                        data.push({key: valueField+submodel, values: values});
                     });
 
-                      data.push({key: valueField+submodel, values: values});
-                    });
+                    this.anim = false;
+                    var valueRange = options.valueRange;
+                    if (this.attr.autoscale){
+                      if (this.maxRangeValue !== maxValue){
+                        this.maxRangeValue = maxValue;
+                        this.anim = true;
+                      }
+                      valueRange = [0, this.maxRangeValue*1.15];
+                    };
                     
                     x.domain(options.range);
-                    y.domain(options.valueRange);
+                    y.domain(valueRange);
 
                     this.options = options;
 
-                    this.updateChart();
-                    
+                    if (!this.initialized){
+                      this.initialized = true;
+                      this.createChart();
+                    }else{
+                      this.updateChart();
+                    }       
                 });
 
                 this.on('actionSelected', function(e, value){
@@ -186,23 +223,21 @@ define(
                     this.trigger('valueChange', this.options);
                 });
 
-                this.showTooltip = function(circle, d) {
-                    var pos = $(circle).offset();
+                this.showTooltip = function(elem, d) {
+
+                    if (!this.attr.tooltip.caption) this.attr.tooltip.caption = '';  
                     
-                    if (!this.attr.tooltip.caption) this.attr.tooltip.caption = '';
+                    d3.select(elem).attr('opacity', 1);
+                    var offset = $(elem).width() / 2;
+                    var hide = function(){
+                         d3.select(elem).attr('opacity', 0);
+                    };
+                    var html = '<div class"value">'+(d.value+d.y0)+'</div><div class="caption">'+this.attr.tooltip.caption+'</div>'; 
+                    var triggerObj = {'html': html, 'elem': elem, 'offset': offset, 'fnHide':  hide, 'elemId': d.date};
+                    this.trigger('showTooltip', triggerObj);
 
-                    this.tooltip.html('<div class"value">'+d.value+'</div><div class="caption">'+this.attr.tooltip.caption+'</div>');
-                    this.tooltip.css({
-                        top: pos.top,
-                        left: pos.left + $(circle).width() / 2
-                    });
-                    this.tooltip.show();
-                    $('.chart rect.bar:first').trigger('mouseenter');
                 };
 
-                this.hideTooltip = function() {
-                    this.tooltip.hide();
-                };
             });
         }
     }

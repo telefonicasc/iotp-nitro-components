adba77b2facb6a9d5447df563fa3e4413b210ea5
Jimmy Collazos <acido69@gmail.com>
2014-02-10 09:37:01 +0100

    task/add-scroll install component

--- components/container.js
+++ components/container.js
@@ -2,11 +2,12 @@ define(
   [
     'components/component_manager',
     'components/mixin/container',
-    'components/mixin/data_binding'
+    'components/mixin/data_binding',
+    'components/mixin/scrollable'
   ],
 
-  function(ComponentManager, ContainerMixin, DataBinding) {
-    return ComponentManager.create('container', ContainerMixin, DataBinding);
+  function(ComponentManager, ContainerMixin, DataBinding, Scrollable) {
+    return ComponentManager.create('container', ContainerMixin, DataBinding, Scrollable);
   }
 
 );

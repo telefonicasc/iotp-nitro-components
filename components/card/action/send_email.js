95a5eacebe0907d96fbbcded3b7cf5eef2747339
etnassoft <etnasdrivers@hotmail.com>
2014-03-20 13:11:50 +0100

    New FROM field in mails

--- components/card/action/send_email.js
+++ components/card/action/send_email.js
@@ -6,9 +6,10 @@ define(
         'components/flippable'
     ],
 
-    function(ComponentManager, DataBinding, Card, Flippable) {
+    function ( ComponentManager, DataBinding, Card, Flippable ) {
 
         var defaultAttrs = {
+            from: '',
             subject:'',
             emailAddress:'',
             message:'',
@@ -27,7 +28,10 @@ define(
             //    to: 'To'
             // }
         };
-        var BACK_TPL = ['<div class="card-header">',
+        var BACK_TPL = [
+            '<div class="card-header">',
+                '<label class="email-from-label">From</label>',
+                '<input class="email-from">',
                 '<label class="email-subject-label">Subject</label>',
                 '<input class="email-subject">',
                 '<label class="email-address-label">To</label>',
@@ -36,16 +40,19 @@ define(
             '<div class="card-body">',
                 '<textarea class="email-message"></textarea>',
             '</div>',
-            '<div class="token-selector"></div>'].join('');
-        var FRONT_TPL = ['<i class="email-address"></i>',
+            '<div class="token-selector"></div>'
+        ].join('');
+        var FRONT_TPL = [
+            '<i class="email-address"></i>',
             '<h3 class="email-subject"></h3>',
-            '<p class="email-message"></p>'].join('') ;
+            '<p class="email-message"></p>'
+        ].join('') ;
         var TOKEN_TPL = '<div class="token"></div>';
         var TOKEN_SYMBOL = '+';
         var TOKEN_VALUE_TPL_START = '${';
         var TOKEN_VALUE_TPL_END = '}';
 
-        function SendEmail() {
+        function SendEmail () {
             // this == scope of component
             this.defaultAttrs( $.extend({},defaultAttrs) );
             this.after('initialize', _install);
@@ -59,6 +66,7 @@ define(
             };
 
             this.getValue = function(){
+                this._userParamsObject['mail.from'] = this.$element.back.from.val();
                 this._userParamsObject['mail.subject'] = this.$element.back.subject.val();
                 this._userParamsObject['mail.to'] = this.$element.back.emailAddress.val();
                 this._userParamsObject['mail.message'] = this.$element.back.message.val();
@@ -77,20 +85,20 @@ define(
 
             this.isValid = function(){
                 var userParam = this._userParamsObject;
-                var a = false;
-                var b = false;
-                var c = false;
-                if(userParam['mail.subject'] && userParam['mail.to'] && userParam['mail.message']){
-                    a = (userParam['mail.subject'].length > 0 );
-                    b = (userParam['mail.to'].length > 0 );
-                    c = (userParam['mail.message'].length > 0 );
+                var a, b, c, d;
+                    a = b = c = d = false;
+                if ( userParam[ 'mail.from' ] && userParam[ 'mail.subject' ] && userParam[ 'mail.to' ] && userParam[ 'mail.message' ] ) {
+                    a = ( userParam[ 'mail.from' ].length > 0 );
+                    b = ( userParam[ 'mail.subject' ].length > 0 );
+                    c = ( userParam[ 'mail.to' ].length > 0 );
+                    d = ( userParam[ 'mail.message' ].length > 0 );
                 }
 
                 return (a && b && c);
             };
         }
 
-        function _install(){
+        function _install () {
             var frontTpl = $(FRONT_TPL);
             var backTpl = $(BACK_TPL);
             // this == scope of element in component
@@ -98,6 +106,7 @@ define(
             this.$back.find('.body').append(backTpl);
             var element = this.$element = {
                 back : {
+                    from : $(backTpl).find('input.email-from'),
                     subject : $(backTpl).find('input.email-subject'),
                     emailAddress : $(backTpl).find('input.email-address'),
                     message : $(backTpl).find('.email-message'),
@@ -127,11 +136,13 @@ define(
             element.back.message.on('change', _updateElementOnChange(element.front.message) );
 
             var triggerUpdateOnChange = _triggerUpdateOnChange(this);
+            element.back.from.on('change',  triggerUpdateOnChange).keyup(triggerUpdateOnChange);
             element.back.emailAddress.on('change',  triggerUpdateOnChange).keyup(triggerUpdateOnChange);
             element.back.subject.on('change', triggerUpdateOnChange ).keyup(triggerUpdateOnChange);
             element.back.message.on('change', triggerUpdateOnChange ).keyup(triggerUpdateOnChange);
 
             //set values
+            element.back.from.val(userParamsObject['mail.from']);
             element.back.subject.val(userParamsObject['mail.subject']);
             element.back.emailAddress.val(userParamsObject['mail.to']);
             element.back.message.val(userParamsObject['mail.message']);
@@ -143,31 +154,37 @@ define(
             $('.email-address-label', this.$node).html(this.attr.locales.to);
 
             var node = this.$node;
-            $(node.parent() ).on('click', function(){
-                node.removeClass('flip');
-            });
+
+            $( node.parent() ).on( 'click', function () {
+                node.removeClass( 'flip' );
+            } );
+
             var insertInMessage = true;
-            $(element.back.subject).focus(function(){
+
+            $( element.back.subject ).focus( function () {
                 insertInMessage = false;
-            });
-            $(element.back.message).focus(function(){
+            } );
+
+            $( element.back.message ).focus( function () {
                 insertInMessage = true;
-            });
-            $(element.back.token).on('click', '.token', function(){
-                var token = $(this).text().replace(TOKEN_SYMBOL,'');
+            } );
+
+            $( element.back.token ).on( 'click', '.token', function () {
+                var token = $( this ).text().replace( TOKEN_SYMBOL,'' );
                 var value = TOKEN_VALUE_TPL_START+token+TOKEN_VALUE_TPL_END;
                 var toElement = element.back.message;
-                if(! insertInMessage ){
+                if ( ! insertInMessage ){
                     toElement = element.back.subject;
                 }
-                _insertAt(toElement[0], value);
+                _insertAt( toElement[ 0 ], value );
                 element.back.message.change();
                 toElement.focus();
-            });
+            } );
 
             this.validate();
         }
-        function _stopPropagation(e){
+
+        function _stopPropagation ( e ) {
             e.stopPropagation();
         }
 
@@ -178,65 +195,69 @@ define(
             };
         }
 
-        function _makeSymbolElement(name){
-            var ele = $(TOKEN_TPL).text(TOKEN_SYMBOL+name);
+        function _makeSymbolElement ( name ) {
+            var ele = $( TOKEN_TPL ).text( TOKEN_SYMBOL + name );
+
             return ele;
         }
 
-        function _updateElementOnChange(elementTo){
-            var callbackToEvent = function(){
-                var value = $(this).val();
+        function _updateElementOnChange ( elementTo ) {
+            var callbackToEvent = function () {
+                var value = $( this ).val();
                 elementTo.text( value );
             };
+
             return callbackToEvent;
         }
 
-        function _triggerUpdateOnChange(card){
-            return function(){
+        function _triggerUpdateOnChange ( card ) {
+            return function () {
                 card.validate();
                 card.valueChange();
             };
         }
 
-        function _insertAt(element, value) {
+        function _insertAt ( element, value ) {
             var sel, startPos, endPos, newPos;
             //IE support
-            if (document.selection) {
+            if ( document.selection ) {
                 element.focus();
                 sel = document.selection.createRange();
                 sel.text = value;
             }
             //MOZILLA and others
-            else if (element.selectionStart || element.selectionStart === '0') {
+            else if ( element.selectionStart || element.selectionStart === '0' ) {
                 startPos = element.selectionStart;
                 endPos = element.selectionEnd;
-                newPos = (startPos + value.length);
-                element.value = [element.value.slice(0, startPos), value, element.value.slice(endPos)].join('');
+                newPos = ( startPos + value.length );
+                element.value = [ element.value.slice( 0, startPos ), value, element.value.slice( endPos ) ].join( '' );
                 element.selectionStart = element.selectionEnd = newPos;
             } else {
                 element.value += value;
             }
         }
 
-        function _userParamsToObject(params){
+        function _userParamsToObject ( params ) {
             var obj = {};
-            $.each(params, function(i,o){
-                obj[o.name] = o.value;
-            });
+            $.each( params, function ( i,o ) {
+                obj[ o.name ] = o.value;
+            } );
+
             return obj;
         }
 
-        function _userParamsObjectToArray(obj){
+        function _userParamsObjectToArray ( obj ) {
             var name, arr=[];
-            for(name in obj){
-                arr.push({
+            for ( name in obj ) {
+                arr.push( {
                     'name': name,
-                    'value':obj[name]
-                });
+                    'value':obj[ name ]
+                } );
             }
+
             return arr;
         }
 
-        return ComponentManager.extend(Card, 'SendEmail', SendEmail, DataBinding);
+        return ComponentManager.extend( Card, 'SendEmail', SendEmail, DataBinding );
     }
 );
